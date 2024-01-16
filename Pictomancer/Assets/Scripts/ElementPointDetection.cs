6973e3b78a2f46e8f1947e9b3d3c37e2b85abaa1
Cha-xeo <arnaud.lalande@epitech.eu>
2024-01-16 03:27:28 +0100

    MousePattern

---
+++ Pictomancer/Assets/Scripts/ElementPointDetection.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pictomancer.Element
+{
+    public class ElementPointDetection : MonoBehaviour
+    {
+        ElementDetection Parent;
+        public int ID;
+        bool completed = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Parent = transform.parent.gameObject.GetComponent<ElementDetection>();
+            Parent.MaxPoint++;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!completed && collision.CompareTag(Constants.TAG_MAGICQUILL))
+            {
+                Parent.PointCompleted++;
+                completed = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag(Constants.TAG_MAGICQUILL))
+            {
+                Parent.PointCompleted--;
+                completed = false;
+            }
+        }
+    }
+}
